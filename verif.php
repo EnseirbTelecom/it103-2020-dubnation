9d98fa7da83f97732a918eede19b72acd57d364d
Pierre-Louis Transon <pltranson@yahoo.fr>
2020-04-23 23:43:09 +0200

    ajout fonction mdp et inclusion dans verif

--- verif.php
+++ verif.php
@@ -1,5 +1,5 @@
 <?php
-	include("function.php");
+	include("functions.php");
   include("login.php");
     session_start();
 ?>
@@ -7,7 +7,17 @@
 <?php
 if (empty($_POST["pseudo"]) || empty($_POST["password"])){
     echo "champs pas remplis";
-    }
+	}
+
+		elseif (isset($_POST["pseudo"]) && isset($_POST["password"])) {
+				 "salut moi c'est saucisse";
+				 $pseudo=$_POST["pseudo"];
+				 $password=$_POST["password"];
+				 echo $pseudo;
+				 echo $password;
+				 checkPassword($pseudo,$password);
+		}
+
 
 
 ?>
