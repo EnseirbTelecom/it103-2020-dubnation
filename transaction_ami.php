1a7dae9c99c473bdf166ca83b7b8ad28e9cc500e
Scialom Zachary <zscialom@enseirb-matmeca.fr>
2020-05-05 20:55:49 +0200

    transaction temporaire

--- transaction_ami.php
+++ transaction_ami.php
@@ -1,62 +1,131 @@
-<?php
-        //Connexion à la base de donnée
-        $server = 'localhost';
-        $username = 'admin';
-        $password = 'it103';
-        $database = 'Dubnation';
-
-        $conn = new mysqli($server, $username, $password, $database);
-
-        if($conn->connect_error) {
-        die('Connection failed: ' . $conn->connect_error . ".\r\n");
-    }
-    echo "Connection successful!\r\n";
-
+<?php 
+	include("functions.php");
+    session_start();
 ?>
 
-<?php
-
-include("function.php");
-
-if (isset($_POST['ami']) && isset($_POST['Montant'])){
-
-  //ON RECOLTE LES INFORMATIONS
-  $userid = $_GET['userid'];
-  $ami = $_POST['ami'];
-  $Montant = $_POST['Montant'];
-  $date = $_POST['date'];
-  $status = $_POST['status'];
-  $contexte = $_POST['contexte'];
-
-  // ON VERIFIE SI LE PSEUDO EST DANS LA TABLE
-  $sql  = "SELECT COUNT(*) AS nbr FROM user WHERE pseudo = '".$ami."'";
-  $res  = mysql_query($sql);
-  $alors  = mysql_fetch_assoc($res);
-
-    // UNE BOUCLE POUR INFORMER L'UTLISATEUR
-    if(isset($ami)){
-      if($alors['nbr'] == 0){
-        echo "Ce pseudo est invalide !";
-      }
-
-  //VERIFIER SI LE SOLDE DE L'UTILISATEUR EST SUFFISANT
-  $pr = "SELECT sum as solde FROM Transaction_Ami WHERE id='".$userid."'";
-  $ver = mysql_query($pr);
-  $ver_table = mysql_fetch_assoc($ver);
-
-   //UNE BOUCLE POUR INFORMER L'UTILISATEUR
-   if(isset($userid)){
-     if($ver_table['solde'] < $montant){
-       echo "Solde insuffisant pour cette opération";
-     }
-     else{
-       $solde = $ver_table['solde'] - $montant;
-     }
-   }
-
-   //MODIFIER LA BASE DE DONNEES
-   $conn->query("INSERT INTO Transaction_Ami ('id','id_user_dept','status','date','contexte','sum') VALUES ('$userid','$Montant','$status','$contexte','$solde')");
-   echo "Paiment effectué";
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+         <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="../it103/signup_css.css">
+		 <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 
-}
+		<title> Transaction </title>
+
+    </head>
+    <body>
+		<div class="container">
+		<div class="row centered-form">
+		<div class="col-xs col-sm col-md col-sm-offset col-md-offset">
+			<div class="panel panel-default">
+				<div class="panel-heading" >
+					<h2 class="panel-title">Réalises une transaction simple vers un ami</h2>
+				</div>
+					<div class="panel-body" id="debut">
+					<form method="post" action="transaction_ami_action.php" >
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <label for="utilisateur_source"> Utilisateur source </label>
+                <input type="text" name="utilisateur_source" id="utilisateur_source" class="form-control input-sm" placeholder="Rentrez votre pseudo" required>
+                  <div class="valid-feedback">
+                      Complétez l'ensemble des champs
+                  </div>
+                  <div class="invalid-feedback">
+                      Veuillez rentrer votre pseudo
+                  </div>
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="utilisateur_cible"> Utilisateur cible </label>
+                <input type="text" name="utilisateur_cible" id="utilisateur_cible" class="form-control input-sm" placeholder="Rentrez le pseudo de votre ami" required>
+                  <div class="valid-feedback">
+                      Complétez l'ensemble des champs
+                  </div>
+                  <div class="invalid-feedback">
+                      Veuillez rentrer le pseudo de votre ami
+                  </div>
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="Message_Explicatif">Contexte</label>
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="email"></span>
+                  </div>
+                  <input type="text" name ="Message_Explicatif" class="form-control input-sm" placeholder="Contexte" required>
+                  <div class="invalid-feedback">
+                      Veuillez rentrer le contexte de cette transaction
+                  </div>
+                </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <label for="Montant">Montant</label>
+                <input type="number" class="form-control" id="Montant" name="Montant" placeholder="Montant >0" required>
+            </div>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="date_de_création"> Date ayant donné lieu à la transaction </label>
+                <input class="form-control input-sm" type="date" name="date_de_création">
+                <div class="invalid-feedback">
+									Veuillez rentrer la date de la transaction
+								</div>
+            </div>
+            </div>
+            <div class="form-row">
+              <label for="Statut">Statut </label>
+                <select class="form-group" id="Statut" name="Statut">
+                    <option value="Ouvert">Ouvert</option>
+                    <option value="Remboursée">Remboursé</option>
+                    <option value="Annulée">Annulé</option>
+                </select>
+              <div class="valid-feedback">Ok !</div>
+              <div class="invalid-feedback">Valeur incorrecte</div>
+            </div>
+            <div class="form-row">
+                <label for="Message_de_fermeture">Message de fermeture</label>
+                <input type="text" class="form-control" id="Message_de_fermeture" name="Message_de_fermeture" placeholder="Merci" required>
+                <div class="valid-feedback">Ok !</div>
+                <div class="invalid-feedback">Valeur incorrecte</div>
+            </div>
+            <div class="form-row">
+                <label for="date_de_fermeture">Date de fermeture</label>
+                <input type="date" class="form-control" id="date_de_fermeture" name="date_de_fermeture" placeholder="Rentrez la date de fermeture" required>
+                <div class="valid-feedback">Ok !</div>
+                <div class="invalid-feedback">Valeur incorrecte</div>
+            </div>
+            <input type="submit" value="Saisir la transaction" id="payer">  
+          </form>  
+         </div>
+         </div>
+         </div>
+         </div>
+         </div>
+         <script>
+           /*La fonction principale de ce script est d'empêcher l'envoi du formulaire si un champ a été mal rempli
+            *et d'appliquer les styles de validation aux différents éléments de formulaire*/
+           (function() {
+             'use strict';
+             window.addEventListener('load', function() {
+               let forms = document.getElementsByClassName('needs-validation');
+               let validation = Array.prototype.filter.call(forms, function(form) {
+                 form.addEventListener('submit', function(event) {
+                   if (form.checkValidity() === false) {
+                     event.preventDefault();
+                     event.stopPropagation();
+                   }
+                   form.classList.add('was-validated');
+                 }, false);
+               });
+             }, false);
+           })();
+         </script>
+      </body>
+
+      </html>
